17abbb6c84b048ecc5368ae7c9bdc164e02258bb
Emmanuel Osamau <151487073+EOsamau@users.noreply.github.com>
2024-09-26 18:35:09 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,39 +1,36 @@
 # %%
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 
-# Load training data
+# %%
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
+TrainData.head()
 
-# Prepare training data
+# %%
+from sklearn.model_selection import train_test_split
+
 Y = TrainData['meal']
-X = TrainData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+X = TrainData.drop(columns=['meal','id','DateTime'], axis=1)
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
-# Create and train the model
-model = DecisionTreeClassifier(max_depth=10, min_samples_leaf=5)
-modelFit = model.fit(x_train, y_train)
+# %%
 
-# Predictions and accuracy
-pred_train = model.predict(x_train)
-pred_test = model.predict(x_test)
+model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state= 42, class_weight = "balanced")
+modelFit = model.fit(x_train,y_train)
 
-train_accuracy = 100 * accuracy_score(y_train, pred_train)
-test_accuracy = 100 * accuracy_score(y_test, pred_test)
+# Test our model using the testing data
+predict = model.predict(x_test)
+acc = accuracy_score(y_test, predict)
 
-print(f'Training Accuracy: {train_accuracy}')
-print(f'Test Accuracy: {test_accuracy}')
+print("Model accuracy is {}%.".format(acc*100))
 
-# Check accuracy threshold (e.g., 70%)
-assert test_accuracy > 70, "Test accuracy is below the expected threshold!"
-
-# Load and predict on test data
+# %%
 TestData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv')
-xt = TestData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+xt = TestData.drop(columns = ['meal','id','DateTime'], axis =1)
 pred = model.predict(xt)
-print(pred)
+pred
 
-# Ensure prediction size matches the input
-assert len(pred) == len(xt), "Prediction length does not match input length!"
+
